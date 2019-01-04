d8cc97a93527a546fbdecaa5b961cc175a47d376
zhangwujuan11 <1145451172@qq.com>
2019-01-04 18:53:43 +0800

    首页店铺展示修改完完成

--- js/comm.js
+++ js/comm.js
@@ -1,6 +1,43 @@
 $(document).ready(function () {
-                $('header').load('top.html');
-                $('footer').load('footer.html');
-            });
+	//头尾公共提取
+        $('header').load('top.html');
+        $('footer').load('footer.html');
+        
+    //首页导航
+    $(".root li").click(function(){
+		$(".tntreebox").css("background","rgba(0,0,0,0.5)")
+		$("#close").show();
+		$(".ex-btn button").hide();
+		var pnum=$(this).index()+1;
+		$(this).addClass("cur");
+		$(this).siblings().removeClass("cur");
+ 		$('.tree2 div').not("#o_"+pnum).hide();
+ 		$('.tree3 div').hide();
+		$("#o_"+pnum).show(function(){
+			$(this).find("li").click(function  () {
+				var nnum=$(this).index()+1;
+				$(this).addClass("cur");
+				$(this).siblings().removeClass("cur");
+				$("#o_"+pnum+"_"+nnum).show();
+				$('.tree3 div').not("#o_"+pnum+"_"+nnum).hide();
+			})
+		})
+	})
+	
+	$("#close").click(function  () {
+		$(".tntreebox").css("background","transparent");
+		$("#close").hide();
+		$(".box").not(".root").hide();
+		$(".root li").removeClass("cur");
+		$(".ex-btn button").hide();
+	})
+	
+	$(".tree3 li").click(function  () {
+		$(".ex-btn button").show();
+		$(this).addClass("cur");
+		$(this).siblings().removeClass("cur");
+	})
+        
+});
 
 
