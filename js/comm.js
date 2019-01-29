6599469f98c8439e150571046ee986c7c5467193
zhangwujuan11 <1145451172@qq.com>
2019-01-29 17:00:35 +0800

    com.jss整合

--- js/comm.js
+++ js/comm.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	//头尾公共提取
-        $('header').load('top.html');
-        $('footer').load('footer.html');
-        
+        $('header').load('/common/top.html');
+        $('footer').load('/common/footer.html');
+
         var bhei= $("body").height();
        if(bhei>=950){
        	$("body,html").css("height","auto");
@@ -11,7 +11,7 @@ $(document).ready(function () {
        	$("body,html").css("height","100%");
        		$("footer").css("bottom","0");
        }
-        
+
     //首页导航
     $(".root li").click(function(){
 		$(".tntreebox").css("background","rgba(0,0,0,0.5)")
@@ -32,7 +32,7 @@ $(document).ready(function () {
 			})
 		})
 	})
-	
+
 	$("#close").click(function  () {
 		$(".tntreebox").css("background","transparent");
 		$("#close").hide();
@@ -40,13 +40,13 @@ $(document).ready(function () {
 		$(".root li").removeClass("cur");
 		$(".ex-btn button").hide();
 	})
-	
+
 	$(".tree3 li").click(function  () {
 		$(".ex-btn button").show();
 		$(this).addClass("cur");
 		$(this).siblings().removeClass("cur");
 	})
-	
+
 	//个人中心手风琴菜单
 	var Accordion = function(el, multiple) {
 		this.el = el || {};
@@ -68,7 +68,7 @@ $(document).ready(function () {
 		}
 	}
 	var accordion = new Accordion($('.accordion'), false);
-	
+
 	$(".link").click(function  () {
 		var puls=$(this).find(".plus").css("display");
 		if (puls == "block") {
@@ -83,54 +83,57 @@ $(document).ready(function () {
 			$(this).parent().parent(".accordion").siblings().find(".jian").hide();
 		}
 	})
-	
+
 	//没有搜索到相关主播
-	setTimeout(function(){ 
+	setTimeout(function(){
 		$("#havenone").fadeOut();
 	}, 2000);
-	
-	
+
+
 	//确认订单
 	$("#jianinpu").click(function  () {
-		var shonum=$("#show-numinpu").val();
+		var shonum=$("#show-numinpu").val()||0;
 		if (shonum > 1) {
-			shonum-=1
+			shonum-=1;
 			$("#jianinpu").css("opacity","1")
 			$("#show-numinpu").val(shonum);
-			var a= parseInt($("#price").html());
-			var b= parseInt($("#show-numinpu").val());
+			var a= parseFloat($("#price").html());
+			var b= parseInt($("#show-numinpu").val())||0;
 			var c=a*b;
 			$("#tognum, .tognum").html(c.toFixed(2));
-			
+
 		} else{
 			$("#show-numinpu").val(1);
 			$("#jianinpu").css("opacity","0.5");
 		}
-	})
+	});
 	$("#jiainpu").click(function  () {
 		var shonwum=parseInt($("#show-numinpu").val())
-			shonwum=shonwum+1
+			shonwum=shonwum+1;
 			$("#show-numinpu").val(shonwum);
 			$("#jianinpu").css("opacity","1");
-			var a= parseInt($("#price").html());
-			var b= parseInt($("#show-numinpu").val());
+			var a= parseFloat($("#price").html());
+			var b= parseInt($("#show-numinpu").val())||0;
 			var c=a*b;
 			$("#tognum, .tognum").html(c.toFixed(2));
-	})
-	
+	});
+
 	$("#show-numinpu").change(function  () {
-		var a= parseInt($("#price").html());
-		var b= parseInt($("#show-numinpu").val());
+		var a= parseFloat($("#price").html());
+		var b= parseInt($("#show-numinpu").val().trim())||0;
+		if(b<=0){
+			b=1;
+			$("#show-numinpu").val(1);
+		}
 		var c=a*b;
 		$("#tognum, .tognum").html(c.toFixed(2));
-	})
-	
-	var a= parseInt($("#price").html());
-	var b= parseInt($("#show-numinpu").val());
+	});
+
+	var a= parseFloat($("#price").html());
+	var b= parseInt($("#show-numinpu").val())||0;
 	var c=a*b;
 	$("#tognum, .tognum").html(c.toFixed(2));
-	
-	
+
 });
 
 
